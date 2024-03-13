e2b4258da457e7fadd2c25bddd71ce395d67eda9
ddohyeong <dohyeong7026@gmail.com>
2024-03-13 19:39:14 +0900

    first_commit

---
+++ src/main/java/jpatestssg/ssgdot/SsgdotApplication.java
@@ -0,0 +1,13 @@
+package jpatestssg.ssgdot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgdotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgdotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpatestssg/ssgdot/SsgdotApplicationTests.java
@@ -0,0 +1,13 @@
+package jpatestssg.ssgdot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgdotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
